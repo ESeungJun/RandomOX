4c1f176b8971bdbf60a8efec2fca8fd97a66646a
이승준 <rbfaosldk@hubilon.com>
2019-01-25 13:43:43 +0900

    fix : 메인화면 추가 / ox 화면에 다음 문제 버튼 추가 / 상단 back 추가

--- app/src/main/java/com/seungjun/randomox/MainActivity.java
+++ app/src/main/java/com/seungjun/randomox/MainActivity.java
@@ -1,10 +1,13 @@
 package com.seungjun.randomox;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -12,16 +15,11 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.btn_o)
-    ImageView btnO;
+    @BindView(R.id.main_start)
+    TextView btnStart;
 
-    @BindView(R.id.btn_x)
-    ImageView btnX;
-
-    int[][] states = new int[][] {
-            new int[] {android.R.attr.state_pressed},
-            new int[] {android.R.attr.state_enabled}
-    };
+    @BindView(R.id.main_title)
+    TextView mainTitle;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,22 +28,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
-
-        btnO.setImageTintList(new ColorStateList(states,
-                new int[] {getResources().getColor(R.color.color_305bb2), getResources().getColor(R.color.color_4482ff)}));
-
-        btnX.setImageTintList(new ColorStateList(states,
-                new int[] {getResources().getColor(R.color.color_b23743), getResources().getColor(R.color.color_ff4f60)}));
     }
 
-    @OnClick(R.id.btn_o)
-    public void clickO(){
-
+    @OnClick(R.id.main_start)
+    public void clickStart(){
+        Intent intent = new Intent(this, OXActivity.class);
+        startActivity(intent);
     }
 
-
-    @OnClick(R.id.btn_x)
-    public void clickX(){
-
-    }
 }
---
+++ app/src/main/java/com/seungjun/randomox/OXActivity.java
@@ -0,0 +1,59 @@
+package com.seungjun.randomox;
+
+import android.content.res.ColorStateList;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class OXActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_o)
+    ImageView btnO;
+
+    @BindView(R.id.btn_x)
+    ImageView btnX;
+
+    int[][] states = new int[][] {
+            new int[] {android.R.attr.state_pressed},
+            new int[] {android.R.attr.state_enabled}
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_ox);
+
+        ButterKnife.bind(this);
+
+        btnO.setImageTintList(new ColorStateList(states,
+                new int[] {getResources().getColor(R.color.color_305bb2), getResources().getColor(R.color.color_4482ff)}));
+
+        btnX.setImageTintList(new ColorStateList(states,
+                new int[] {getResources().getColor(R.color.color_b23743), getResources().getColor(R.color.color_ff4f60)}));
+    }
+
+    @OnClick(R.id.btn_o)
+    public void clickO(){
+
+    }
+
+
+    @OnClick(R.id.btn_x)
+    public void clickX(){
+
+    }
+
+
+    @OnClick(R.id.top_back)
+    public void back(){
+        finish();
+    }
+
+}
